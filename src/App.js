813d70ffd49244b6663ea3177fdec40fb230ab5f
david <davidlucky@gmail.com>
2021-10-23 17:22:26 -0500

    add structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box, Typography, Button, Grid } from "@mui/material";
+import styled from "styled-components";
+import Header from "./components/Header";
+
+const Container = styled.div`
+	&.container {
+		width: 90%;
+		margin: 0 auto;
+		background: salmon;
+	}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Container className="container">
+			<Box className="box">
+				<Typography variant="h1">Hola Mundo</Typography>
+				<Button variant="contained">Login</Button>
+			</Box>
+			<Header></Header>
+		</Container>
+	);
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import { Box } from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+const Header = () => {
+	return (
+		<Box>
+			<p>Medellin, Colombia</p>
+			<KeyboardArrowDownIcon />
+			import KeyboardArrowDownIcon from
+			'@mui/icons-material/KeyboardArrowDown';
+			<p>Ingresar direccion</p>
+			<p>Tus pedidos</p>
+			<p>
+				Registrate o <br /> iniciar sesion
+			</p>
+		</Box>
+	);
+};
+
+export default Header;
